52d76f603fd4df1bad9504cb62d89d430d711243
FedericoCano <fedecano1988@gmail.com>
2024-09-03 13:18:07 -0600

    correccion script barrios

--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
     const localidades = [
-        // Lista de localidades...
+        "Ciudad de Buenos Aires",
+        "Acassuso", "Aldo Bonzi", "Avellaneda", "Ballester", "Banfield", "Beccar", "Belén de Escobar", 
+        "Benavídez", "Bernal", "Billinghurst", "Bosques", "Boulogne", "Canning", "Carupá", "Caseros", 
+        "Castelar", "Ciudad Evita", "Ciudadela", "Del Viso", "Dock Sud", "Don Bosco", "Don Torcuato", 
+        "El Jagüel", "El Libertador", "El Palomar", "Escalada", "Ezpeleta", "Fátima", "Florida", 
+        "Florencio Varela", "Garín", "Gerli", "Gobernador Costa", "González Catán", "Grand Bourg", 
+        "Haedo", "Hudson", "Hurlingham Centro", "Ingeniero Adolfo Sourdeaux", "Ingeniero Allan", 
+        "Ingeniero Maschwitz", "Ingeniero Pablo Nogués", "Isidro Casanova", "Ituzaingó Norte", 
+        "Ituzaingó Sur", "José C. Paz Centro", "José León Suárez", "La Lucila", "La Matanza", 
+        "La Tablada", "Lanús Este", "Lanús Oeste", "Llavallol", "Loma Hermosa", "Lomas de Zamora", 
+        "Los Polvorines", "Luján", "Malvinas Argentinas", "Manuel Alberti", "Martínez", "Matheu", 
+        "Maquinista Savio", "Merlo Centro", "Monte Grande", "Morón", "Munro", "Olivos", 
+        "Parque San Martín", "Piñeiro", "Plátanos", "Quilmes Oeste", "Quilmes", "Ramos Mejía", 
+        "Ranelagh", "Recoleta", "Remedios de Escalada", "Rincón de Milberg", "San Andrés", 
+        "San Antonio de Padua", "San Fernando", "San Isidro", "San Justo", "San Martín", "San Miguel", 
+        "San Vicente", "Sarandí", "Santos Lugares", "Sourigues", "Tapiales", "Temperley", 
+        "Tigre Centro", "Tortuguitas", "Tristán Suárez", "Tres de Febrero", "Troncos del Talar", 
+        "Turdera", "Vicente López", "Victoria", "Villa Adelina", "Villa Ballester", "Villa Bosch", 
+        "Villa Brown", "Villa Dominico", "Villa España", "Villa La Florida", "Villa Luzuriaga", 
+        "Villa Lynch", "Villa Madero", "Villa Martelli", "Villa Raffo", "Villa Sarmiento", 
+        "Villa Tesei", "Villa Udaondo", "Virreyes", "Wilde", "William Morris"
     ];
 
     const localidadesCABA = [
-        // Lista de barrios de CABA...
+        "Belgrano", "Núñez", "Liniers", "Palermo", "Recoleta", "Villa Urquiza", "Almagro", "Caballito", 
+        "San Telmo", "Flores", "Boedo", "Retiro", "Monserrat", "Barracas", "Constitución", "Chacarita", 
+        "Villa Devoto", "Villa del Parque", "Villa Lugano", "Parque Patricios", "Puerto Madero"
     ];
 
     const localidadSelect = document.getElementById('localidad');
@@ -114,7 +136,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
-
 
 
 
