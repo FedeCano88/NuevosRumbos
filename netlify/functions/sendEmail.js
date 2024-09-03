3f5065360220ee82e18423eca3178bfd24f9da5a
FedericoCano <fedecano1988@gmail.com>
2024-09-03 13:32:49 -0600

    mail barrios2

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -6,7 +6,10 @@ exports.handler = async (event, context) => {
         'Access-Control-Allow-Headers': 'Content-Type'
     };
 
+    console.log("Request received:", event);
+
     if (event.httpMethod !== 'POST') {
+        console.log("Invalid HTTP method:", event.httpMethod);
         return {
             statusCode: 405,
             headers,
@@ -14,21 +17,24 @@ exports.handler = async (event, context) => {
         };
     }
 
-    const { nombreApellido, email, telefono, mensaje } = JSON.parse(event.body);
+    // Capturar todos los datos del formulario, incluyendo obra social
+    const { nombreApellido, email, telefono, mensaje, localidad, barrioCABA, localidadServicio, barrioCABAServicio, obraSocial } = JSON.parse(event.body);
+    console.log("Parsed request body:", { nombreApellido, email, telefono, mensaje, localidad, barrioCABA, localidadServicio, barrioCABAServicio, obraSocial });
 
     let transporter = nodemailer.createTransport({
         host: 'smtp-mail.outlook.com',
         port: 587,
-        secure: false, // true para 465, false para otros puertos
+        secure: false,
         auth: {
-            user: process.env.EMAIL_USER, // tu cuenta de Hotmail
-            pass: process.env.EMAIL_PASS  // la nueva contraseña de aplicación
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
         },
         tls: {
-            ciphers: 'SSLv3' // Asegura el uso de un protocolo seguro
+            ciphers: 'SSLv3'
         }
     });
 
+    // Incluir todos los campos, incluyendo obra social, en el cuerpo del correo
     const mailOptions = {
         from: process.env.EMAIL_USER,
         to: 'fedecano1988@hotmail.com', // Reemplaza con tu dirección de correo de destino
@@ -37,22 +43,26 @@ exports.handler = async (event, context) => {
             Nombre y Apellido: ${nombreApellido}
             Email: ${email}
             Teléfono: ${telefono}
-            Mensaje: ${mensaje}
             Localidad: ${localidad}
             Barrio (CABA): ${barrioCABA}
             Localidad del Servicio: ${localidadServicio}
             Barrio del Servicio (CABA): ${barrioCABAServicio}
+            Obra Social: ${obraSocial}
+            Mensaje: ${mensaje}
         `
     };
 
     try {
+        console.log("Attempting to send email...");
         await transporter.sendMail(mailOptions);
+        console.log("Email sent successfully.");
         return {
             statusCode: 200,
             headers,
             body: JSON.stringify({ message: 'Correo enviado con éxito.' })
         };
     } catch (error) {
+        console.error("Error sending email:", error);
         return {
             statusCode: 500,
             headers,
@@ -60,3 +70,5 @@ exports.handler = async (event, context) => {
         };
     }
 };
+
+
